89afcc403d7ae0bb688542c64160032e74f83cb1
Artem Sapegin <artem@sapegin.ru>
2017-05-10 22:25:31 +0200

    Add links to Netlify and site repo to the footer

--- templates/components/Footer.jsx
+++ templates/components/Footer.jsx
@@ -5,7 +5,7 @@ import s from './Footer.pcss';
 export default function Footer() {
 	return (
 		<Block component="header" bottom={1 / 2} class={s.root}>
-			Made with coffee in Berlin by <Link href="http://sapegin.me/">Artem Sapegin</Link> and <Link href="https://github.com/styleguidist/react-styleguidist/graphs/contributors">amazing contributors</Link>. Logo by <Link href="http://okonet.ru/">Andrey Okonetchnikov</Link> and <Link href="http://iamsaravieira.com/">Sara Vieira</Link>
+			Made with coffee in Berlin by <Link href="http://sapegin.me/">Artem Sapegin</Link> and <Link href="https://github.com/styleguidist/react-styleguidist/graphs/contributors">amazing contributors</Link>. Logo by <Link href="http://okonet.ru/">Andrey Okonetchnikov</Link> and <Link href="http://iamsaravieira.com/">Sara Vieira</Link>. Hosted on <Link href="https://www.netlify.com/">Netlify</Link>. <Link href="https://github.com/styleguidist/site">Site sources</Link>
 		</Block>
 	);
 }
