dd5322576a7b45e27626c246e1335f882733413e
Artem Sapegin <artem@sapegin.ru>
2017-05-17 14:22:21 +0200

    Rewrite main page copy again

--- templates/Index.jsx
+++ templates/Index.jsx
@@ -2,15 +2,13 @@ import Base from './Base';
 import DocsButtons from './components/DocsButtons';
 import Examples from './components/Examples';
 import Features from './components/Features';
-import SmallFeatures from './components/SmallFeatures';
 import Splash from './components/Splash';
 
-export default function({ title, subtitle, features, smallFeatures, examples }) {
+export default function({ title, subtitle, features, examples }) {
 	return (
 		<Base>
 			<Splash title={title} subtitle={subtitle} />
 			<Features features={features} />
-			<SmallFeatures features={smallFeatures} />
 			<Examples examples={examples} />
 			<DocsButtons />
 		</Base>
--- templates/components/Features.jsx
+++ templates/components/Features.jsx
@@ -6,18 +6,21 @@ import s from './Features.pcss';
 export default function Features({ features }, children, { typo, typoTitle }) {
 	return (
 		<section>
-			{features.map(({ caption, body, images }) => (
-				<Block bottom={8} class={s.item}>
+			{features.map(({ id, caption, intro, items, images }) => (
+				<Block id={id} bottom={8} class={s.item}>
 					<Layout>
 						<Layout sm={1 / 3} class={s.body}>
 							<Beta class={s.heading}>{typoTitle(caption)}</Beta>
+							<div class={s.intro}>{typo(intro)}</div>
 							<ul>
-								{body.map(text => <li class={s.listItem}>{typo(text)}</li>)}
+								{items.map(text => <li class={s.listItem}>{typo(text)}</li>)}
 							</ul>
 						</Layout>
 						{images.map(image => ( // 1 or 2 images — 2/3 or 1/3 each
 							<Layout sm={(3 - images.length) / 3}>
-								<img class={s.image} src={image} />
+								<div class={s.imageContainer}>
+									<img class={s.image} src={image} />
+								</div>
 							</Layout>
 						))}
 					</Layout>
