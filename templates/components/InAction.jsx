24d440176d383d0b484ccc8e6fac9260dc8d1e16
Artem Sapegin <artem@sapegin.ru>
2017-06-12 20:45:14 +0200

    Remove unused component

--- templates/components/InAction.jsx
+++ templates/components/InAction.jsx
@@ -1,9 +1,8 @@
 import Block from 'tamia/lib/components/Block';
-import s from './SmallFeatures.pcss';
 
 export default function InAction({ features }, children, { typo }) {
 	return (
-		<Block bottom={2} class={s.root}>
+		<Block bottom={2}>
 			<ul>
 				{features.map(feature => (
 					<li>{typo(feature)}</li>
