31207d2a76a2f6ad3e01f945021ba686868037e4
Artem Sapegin <artem@sapegin.ru>
2017-06-16 13:21:06 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,14 +8,14 @@ REPO_DIR="react-styleguidist-master"
 # Download and unpack master branch
 echo
 echo "Downloading react-styleguidist..."
-curl "$REPO_TAR_GZ" | tar xz
+curl "$REPO_TAR_GZ" | tar xz || exit 1
 
 # Build a basic example
 echo
 echo "Building a basic example..."
 cd $REPO_DIR
-npm
-npm run build
+npm install || exit 1
+npm run build || exit 1
 cd -
 
 # Copy to the public folder
@@ -33,5 +33,5 @@ cp $REPO_DIR/docs/* docs
 # Build the site
 echo
 echo "Building the site..."
-npm run build
-npm run bundle
+npm run build || exit 1
+npm run bundle || exit 1
