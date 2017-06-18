510685a371c8baa422ba909e06cb1be57917cb66
Artem Sapegin <artem@sapegin.ru>
2017-06-18 10:33:23 +0200

    Fix spaces between text

--- templates/components/Footer.jsx
+++ templates/components/Footer.jsx
@@ -7,17 +7,12 @@ export default function Footer() {
 	return (
 		<div>
 			<Container component="footer" class={s.root}>
-        Made with coffee in Berlin by{' '}
-				<Link href="http://sapegin.me/">Artem Sapegin</Link> and
-        <Link href="https://github.com/styleguidist/react-styleguidist/graphs/contributors">
-          amazing contributors
-        </Link>. Logo by{' '}
-				<Link href="http://okonet.ru/">Andrey Okonetchnikov</Link> and{' '}
-				<Link href="http://iamsaravieira.com/">Sara Vieira</Link>. Hosted
-        on <Link href="https://www.netlify.com/">Netlify</Link>.
-        <Link href="https://github.com/styleguidist/site">
-          Site sources
-        </Link>
+				Made with coffee in Berlin by <Link href="http://sapegin.me/">Artem Sapegin</Link> and{' '}
+				<Link href="https://github.com/styleguidist/react-styleguidist/graphs/contributors"> amazing contributors</Link>.
+				Logo: <Link href="http://okonet.ru/">Andrey Okonetchnikov</Link> and{' '}
+				<Link href="http://iamsaravieira.com/">Sara Vieira</Link>.
+				Hosting: <Link href="https://www.netlify.com/">Netlify</Link>.{' '}
+				<Link href="https://github.com/styleguidist/site">Site sources</Link>
 			</Container>
 			<script
 				src="//cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"
